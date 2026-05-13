5b61f8a1cd47a1a2a536f554970b30efd422f64e
JennyNakstad <jena08@icloud.com>
2026-05-13 13:03:03 +0200

    js aktiviteter

--- script.js
+++ script.js
@@ -3,3 +3,8 @@ console.log("hei")
 function klikkDestinasjon(by) {
     document.getElementById("destinasjonendin").textContent = by
 }
+
+function selectCard(card) {
+    card.classList.toggle("selected")
+    document.getElementById("aktivitetendin").textContent = card
+}
