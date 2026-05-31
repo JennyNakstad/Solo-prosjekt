8f590dd6e440fb67a8b165df93bcc65295f43f96
JennyNakstad <jena08@icloud.com>
2026-06-01 01:34:04 +0200

    index aktivitet js

--- script.js
+++ script.js
@@ -8,11 +8,11 @@ menuToggle.addEventListener("click", function () {
 
     nav.classList.toggle("active")
 
-    if(nav.classList.contains("active")){
+    if (nav.classList.contains("active")) {
         icon.classList.remove("fa-bars")
         icon.classList.add("fa-xmark")
     }
-    else{
+    else {
         icon.classList.remove("fa-xmark")
         icon.classList.add("fa-bars")
     }
@@ -29,6 +29,22 @@ function klikkDestinasjon(element, by) {
     })
 
     element.classList.add("selected")
+
+    document.getElementById("selectbarcelona").classList.add("hidden")
+    document.getElementById("selectmadrid").classList.add("hidden")
+    document.getElementById("selectsevilla").classList.add("hidden")
+
+    if (by === "Barcelona") {
+        document.getElementById("selectbarcelona").classList.remove("hidden")
+    }
+
+    if (by === "Madrid") {
+        document.getElementById("selectmadrid").classList.remove("hidden")
+    }
+
+    if (by === "Sevilla") {
+        document.getElementById("selectsevilla").classList.remove("hidden")
+    }
 }
 
 
